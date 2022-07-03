2c3e3a7bf0b810a07ff33354e5d5ee2a29dafb16
dhruvbhardwaj14 <dhruv9891891827@gmail.com>
2022-07-03 20:41:49 +0530

    Initial Commit

---
+++ MoonShot/MoonShotApp.swift
@@ -0,0 +1,17 @@
+//
+//  MoonShotApp.swift
+//  MoonShot
+//
+//  Created by Dhruv Bhardwaj on 03/07/22.
+//
+
+import SwiftUI
+
+@main
+struct MoonShotApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
